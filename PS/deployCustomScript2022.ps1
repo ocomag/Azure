1bff9907489d0513e615d6e80f8f67085faf2761
patrick-pfammatter <145437916+patrick-pfammatter@users.noreply.github.com>
2023-10-27 11:14:35 +0200

    Add files via upload

--- PS/deployCustomScript2022.ps1
+++ PS/deployCustomScript2022.ps1
@@ -1,11 +1,19 @@
+# Überprüfen, ob das Verzeichnis C:\Service existiert, wenn nicht, dann erstellen
+if (-not (Test-Path "C:\Service")) {
+    New-Item -Path "C:\Service" -ItemType Directory
+}
+
 # Definieren Sie den Pfad, wohin das Zip-File heruntergeladen werden soll
 $zipFilePath = "C:\Service\CustomServerScript2022.zip"
 
 # Herunterladen des Zip-Files
-Invoke-WebRequest -Uri "URL_TO_YOUR_ZIP_FILE_IN_GITHUB" -OutFile $zipFilePath
+Invoke-WebRequest -Uri "https://github.com/ocomag/Azure/raw/main/Custom/CustomServerScript2022.zip" -OutFile $zipFilePath
 
 # Entpacken des Zip-Files nach C:\Service
 Expand-Archive -Path $zipFilePath -DestinationPath "C:\Service"
 
+# Löschen des heruntergeladenen Zip-Files nach dem Entpacken
+Remove-Item -Path $zipFilePath
+
 # Ausführen der server_customization.cmd
-Start-Process -NoNewWindow -FilePath "C:\Service\server_customization.cmd"
+Start-Process -NoNewWindow -FilePath "C:\Service\Custom Server Script 2022\server_customization.cmd"
